e192a9b972e20d3eb1b3fbfe7392ade96b426942
Adeel Ahmed <ahmedadeel783@gmail.com>
2023-08-12 16:10:41 +0500

    added cannons

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -8,8 +8,9 @@ public class PlayerMovement : MonoBehaviour
     [Header("Movement Variables")]
     [SerializeField] private Joystick joystick;
     private Tween tweenID;
-    private int rotateAngleY = 0, rotateAngleX = 90;
-    
+    public int currentRotateAngleY, currentRotateAngleX;
+
+    public int maxRotateAngleY, minRotateAngleY, maxRotateAngleX, minRotateAngleX;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,21 +20,21 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (joystick.Direction.x < 0 && rotateAngleY < 30f)
+        if (joystick.Direction.x < 0 && currentRotateAngleY < 30f)
         {
-            tweenID = transform.DORotate( new Vector3(rotateAngleX, 0f, rotateAngleY++), 0f);
+            tweenID = transform.DORotate( new Vector3(currentRotateAngleX, 0f, currentRotateAngleY++), 0f);
         } 
-        else if (joystick.Direction.x > 0 && rotateAngleY > -30f)
+        else if (joystick.Direction.x > 0 && currentRotateAngleY > -30f)
         {
-            tweenID = transform.DORotate( new Vector3(rotateAngleX, 0f, rotateAngleY--), 0f);
+            tweenID = transform.DORotate( new Vector3(currentRotateAngleX, 0f, currentRotateAngleY--), 0f);
         }
-        if (joystick.Direction.y < 0 && rotateAngleX < 110f)
+        if (joystick.Direction.y < 0 && currentRotateAngleX < 110f)
         {
-            tweenID = transform.DORotate( new Vector3(rotateAngleX++, 0f, rotateAngleY), 0f);
+            tweenID = transform.DORotate( new Vector3(currentRotateAngleX++, 0f, currentRotateAngleY), 0f);
         } 
-        else if (joystick.Direction.y > 0 && rotateAngleX > 85f)
+        else if (joystick.Direction.y > 0 && currentRotateAngleX > 85f)
         {
-            tweenID = transform.DORotate( new Vector3(rotateAngleX--, 0f, rotateAngleY), 0f);
+            tweenID = transform.DORotate( new Vector3(currentRotateAngleX--, 0f, currentRotateAngleY), 0f);
         }
     }
 }
