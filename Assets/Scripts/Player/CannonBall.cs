71e72ba26ad5d70a15720bdf89793fa21807b2ff
Wasi Hyder <wasihaider05@gmail.com>
2023-08-17 17:36:09 +0500

    Cannon Ball Script Added

--- Assets/Scripts/Player/CannonBall.cs
+++ Assets/Scripts/Player/CannonBall.cs
@@ -7,6 +7,10 @@ public class CannonBall : MonoBehaviour
 {
     private Action<CannonBall> killAction;
     public Color ballColor;
+    public GameObject greenEnemy;
+    public GameObject yellowEnemy;
+    public GameObject blueEnemy;
+    public GameObject redEnemy;
 
     public void Init(Action<CannonBall> killAction)
     {
@@ -15,6 +19,49 @@ public class CannonBall : MonoBehaviour
 
     private void OnCollisionEnter(Collision other)
     {
+        if(other.gameObject.layer == LayerMask.NameToLayer("Enemy"))
+        {
+            other.gameObject.TryGetComponent<Color>(out Color enemyColor);
+
+            if (enemyColor.Equals(ballColor))
+            {
+                //Destroy Enemy Group
+
+                Destroy(other.gameObject.transform.parent.gameObject);
+            }
+            else if (!enemyColor.Equals(ballColor))
+            {
+                //Instaniate Group of Ball Color
+
+                Instantiate(EnemyGroupToSpawn(), other.gameObject.transform.position, other.gameObject.transform.rotation);
+
+            }
+
+        }
+
         if(other.gameObject.layer == LayerMask.NameToLayer("Enemy") || other.gameObject.layer == LayerMask.NameToLayer("Plane")) killAction(this);
+    }
+
+
+    private GameObject EnemyGroupToSpawn()
+    {
+        if (ballColor.Equals(greenEnemy.GetComponentInChildren<Color>()))
+        {
+            return greenEnemy;
+        }
+        else if (ballColor.Equals(redEnemy.GetComponentInChildren<Color>()))
+        {
+            return redEnemy;
+        }
+        else if (ballColor.Equals(blueEnemy.GetComponentInChildren<Color>())) 
+        {
+            return blueEnemy;
+        }
+        else if (ballColor.Equals(yellowEnemy.GetComponentInChildren<Color>()))
+        {
+            return yellowEnemy;
+        }
+
+        return null;
     }
 }
