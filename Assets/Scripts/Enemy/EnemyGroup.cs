2ac4bb62d58eadb91470f51acb382e7355d51e40
Wasi Hyder <wasihaider05@gmail.com>
2023-08-26 04:15:06 +0500

    Lose Condition Set + Enemy Speed Slowed

--- Assets/Scripts/Enemy/EnemyGroup.cs
+++ Assets/Scripts/Enemy/EnemyGroup.cs
@@ -1,12 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class EnemyGroup : MonoBehaviour
 {
+    public static int enemyGroupCount = 0;
     public Transform Cannon;
     [SerializeField] private ParticleSystem blood;
     [SerializeField] private List<GameObject> soldiers;
+    public GameManager gameManager;
+
+    public void Start()
+    {
+        enemyGroupCount = enemyGroupCount + 1;
+    }
+
     public void Die()
     {
         foreach (var soldier in soldiers)
@@ -14,6 +24,12 @@ public class EnemyGroup : MonoBehaviour
             soldier.SetActive(false);
         }
         blood.Play();
+        enemyGroupCount--;
+
+        if (enemyGroupCount == 0)
+        {
+            gameManager.GameWon();
+        }
         Destroy(this);
     }
 }
